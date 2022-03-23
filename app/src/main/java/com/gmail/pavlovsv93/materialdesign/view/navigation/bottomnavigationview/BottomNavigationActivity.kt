dfae8f7bc3c60cf30910310f1f03331eed545afa
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-03-23 11:15:40 +0300

    Привязан фрагмент к меню BottomNavigationView и задан фрагмент по умолчанию

--- app/src/main/java/com/gmail/pavlovsv93/materialdesign/view/navigation/bottomnavigationview/BottomNavigationActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/materialdesign/view/navigation/bottomnavigationview/BottomNavigationActivity.kt
@@ -2,15 +2,51 @@ package com.gmail.pavlovsv93.materialdesign.view.navigation.bottomnavigationview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.gmail.pavlovsv93.materialdesign.R
 import com.gmail.pavlovsv93.materialdesign.databinding.ActivityBottomNavigationBinding
+import com.gmail.pavlovsv93.materialdesign.model.theme.ThemeStorage
+import com.gmail.pavlovsv93.materialdesign.view.navigation.viewpager.EarthFragment
+import com.gmail.pavlovsv93.materialdesign.view.navigation.viewpager.MarsFragment
+import com.gmail.pavlovsv93.materialdesign.view.navigation.viewpager.SolarSystemFragment
 
 class BottomNavigationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBottomNavigationBinding
 
+    private val themeStorage: ThemeStorage = ThemeStorage(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(themeStorage.getTheme().theme)
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.abnBottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_earth -> {
+                    showFragment(EarthFragment.newInstance())
+                    true
+                }
+                R.id.menu_mars -> {
+                    showFragment(MarsFragment.newInstance())
+                    true
+                }
+                R.id.menu_solar_system -> {
+                    showFragment(SolarSystemFragment.newInstance())
+                    true
+                }
+                else -> false
+            }
+        }
+        // todo по умолчанию задать активное меню
+        binding.abnBottomNavigationView.selectedItemId = R.id.menu_mars
+    }
+
+    private fun showFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.abn_fragment_container, fragment)
+            .commit()
+
     }
 }
