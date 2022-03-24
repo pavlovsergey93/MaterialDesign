b402dda648dd7d2d818b0c8a42f7a5e0c4fe776a
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-03-24 12:41:07 +0300

    Создал ConstraintFragment и xml, связал их

--- app/src/main/java/com/gmail/pavlovsv93/materialdesign/view/layouts/ConstraintFragment.kt
+++ app/src/main/java/com/gmail/pavlovsv93/materialdesign/view/layouts/ConstraintFragment.kt
@@ -1,4 +1,37 @@
 package com.gmail.pavlovsv93.materialdesign.view.layouts
 
-class ConstraintFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gmail.pavlovsv93.materialdesign.databinding.FragmentConstraintBinding
+
+class ConstraintFragment: Fragment() {
+
+    private var _binding :FragmentConstraintBinding? = null
+    private val binding get() = _binding!!
+
+    companion object{
+        fun newInstance() = ConstraintFragment()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentConstraintBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
